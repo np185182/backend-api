790bfea55303e700a90fd1b07191841dc0c45f03
ab185588ncr <ab185588@ncr.com>
2023-01-24 14:55:28 +0530

    Second commit with documentation

--- src/controllers/OrderTrend.controller.ts
+++ src/controllers/OrderTrend.controller.ts
@@ -6,7 +6,11 @@ import {OrderTrendService} from '../providers/orderTrend.service'
 @Controller('OrderTrends')
 export class OrderTrendController {
   constructor(private readonly orderTrendService : OrderTrendService){}
-
+    /**
+   * Fetches OrderData from Database upto given number of days 
+   * @param params params consists days which is an numberic type
+   * @returns Json Array of OrderData DTO
+   */
   @Get('/days/:days')
   async getSpecificDaysOrdersData(@Param() params) {
     return this.orderTrendService.getLastXDays(params.days);
