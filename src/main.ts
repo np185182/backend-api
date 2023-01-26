721a17abc374f3cb0ac9d159df99adaaeee7de38
nikhil185182 <119924090+nikhil185182@users.noreply.github.com>
2023-01-26 17:33:06 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './order.module';
-// import * as dotenv from 'dotenv';
 
-// dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
