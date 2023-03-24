893de746996319bb8a36d4089156696685757dbe
sai-000 <vs185268@ncr.com>
2023-03-24 17:26:43 +0530

    x

--- src/resolvers/dashboard.resolver.ts
+++ src/resolvers/dashboard.resolver.ts
@@ -19,8 +19,8 @@ export class OrdertrendResolver {
   }
 
   @Query(()=> [getInactiveUsersData],{name: 'inactiveusers'})
-  GetInactiveUsers(@Args('days', { type: () => Date }) days: Date){
-    return this.ordertrendService.InactiveUsers(days);
+  GetInactiveUsers(@Args('date', { type: () => Date }) date: Date){
+    return this.ordertrendService.InactiveUsers(date);
   }
 
   @Query(() => [companyLevel], { name: 'getSpecificCompanyData' }) getData(
